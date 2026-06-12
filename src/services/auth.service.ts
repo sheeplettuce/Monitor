0a8790d4c3411c8e79d34cbcdf3bf412ad7a3e9c
anonlettuce <migthysheepdev@gmail.com>
2026-06-11 20:42:06 -0600

    cambié bycrypt por bycryptjs porque es un lloronsote este en serio que llorón

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { prisma } from "../config/prisma.js";
 import { logger } from "../utils/logger.js";
--- src/services/usuarios.service.ts
+++ src/services/usuarios.service.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { prisma } from "../config/prisma.js";
 import { logger } from "../utils/logger.js";
 
