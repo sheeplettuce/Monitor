e84a6dad69c034513ba111e3664a1611a9dd8fa2
anonlettuce <migthysheepdev@gmail.com>
2026-06-15 17:11:15 -0600

    prisma errores

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,10 +1,13 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
 
 console.log("PRISMA DATABASE_URL =", process.env.DATABASE_URL);
 
-const adapter = new PrismaPg({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
 });
+
+const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({ adapter });
