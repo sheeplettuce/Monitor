dd115b340e1b081664b9aaf6b1ee3ee39222f112
anonlettuce <migthysheepdev@gmail.com>
2026-06-15 18:28:37 -0600

    arreglo del server

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,13 +1,3 @@
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
 
-console.log("PRISMA DATABASE_URL =", process.env.DATABASE_URL);
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
-});
-
-const adapter = new PrismaPg(pool);
-
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient();
